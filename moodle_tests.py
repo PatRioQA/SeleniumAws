c5ec85f67a2c8763a05d012f2b2aea69d258e6b1
PatRioQA <Patrick.Ordinario0819@mycanctb.ca>
2022-02-16 17:10:52 -0800

    moodle_tests.py is added

---
+++ moodle_tests.py
@@ -0,0 +1,19 @@
+import unittest
+import moodle_methods as methods
+import moodle_locators as locators
+
+class MoodleAppPositiveTestCases(unittest.TestCase):
+
+    def teast_create_new_user(self):
+        methods.setUp()
+        methods.log_in(locators.moodle_username, locators.moodle_password)
+        methods.create_new_user()
+        methods.check_user_created()
+        methods.log_out()
+        methods.log_in(locators.new_username,locators.new_password)
+        methods.check_we_logged_in_with_new_cred()
+        methods.log_out()
+        methods.log_in(locators.moodle_username, locators.moodle_password)
+        methods.delete_a_new_user()
+        methods.log_out()
+        methods.tearDown()
